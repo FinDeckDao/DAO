136797413cb335f9267be724e36d62fe9257a546
jfgrissom <jfgrissom@gmail.com>
2024-08-26 21:18:47 -0700

    feat(proposal): add mock data for ui dev

--- src/Screens/Proposals/index.tsx
+++ src/Screens/Proposals/index.tsx
@@ -1,5 +1,20 @@
 import { FC } from 'react'
 
 export const Proposals: FC = () => {
-  return <>Proposals Screen</>
+  // TODO: This data will be replaced with a call to the backend canister.
+  const data = [
+    { id: 1, title: 'Proposal 1', description: 'This is a proposal' },
+    { id: 2, title: 'Proposal 2', description: 'This is a proposal' },
+    { id: 3, title: 'Proposal 2', description: 'This is a proposal' },
+  ]
+
+  const renderProposals = () => {
+    return data.map((proposal, index) => {
+      return <div key={index} className="">
+        {proposal.id} {proposal.title} {proposal.description}
+      </div>
+    })
+  }
+
+  return <>{renderProposals()}</>
 }
